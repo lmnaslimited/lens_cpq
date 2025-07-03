03c9f260e398d456ffef0ee2b4334b264ea34943
Eswari-Manie <eswari.maniyarasan@lmnas.com>
2025-07-03 07:33:12 +0000

    Slider and UI appearance in Design form

--- lens_cpq/cpq/doctype/design/design.js
+++ lens_cpq/cpq/doctype/design/design.js
@@ -1,4 +1,4 @@
-let InvalidValue = new Set()
+let lInvalidValue = new Set()
 frappe.ui.form.on('Design', {
     refresh(frm) {
         if (frm.doc.design_template) {
@@ -11,26 +11,26 @@ frappe.ui.form.on('Design', {
     },
 
     validate(frm) {
-        if (InvalidValue && InvalidValue.size > 0) {
+        if (lInvalidValue && lInvalidValue.size > 0) {
             frappe.throw(__('Please correct all validation errors before saving.'));
         }
     },
 
+    // Fetches field data from the server and renders the input fields
     fnFetchAndRenderTemplateFields(frm) {
         frappe.call({
             method: "lens_cpq.cpq.doctype.design.api.fn_get_formated_item_variants",
             args: { item_name: frm.doc.design_template },
             callback: function (ldResponse) {
                 if (!ldResponse.message) return;
-                console.log("message", ldResponse.message)
                 const LaFields = frm.events.fnMapFieldswithValues(frm, ldResponse.message);
-                console.log("LaFields", LaFields)
                 const LrenderHtml = frappe.render(frm.events.fnGenerateTemplateHtml(), { LaFields });
-                console.log("LrenderHtml", LrenderHtml)
                 frm.events.fnRenderDynamicSlider(frm, LrenderHtml, LaFields);
             }
         });
     },
+
+    // Maps each field with its saved value or fallback to default/min
     fnMapFieldswithValues(frm, iaFields) {
         return iaFields.map(ldField => ({
             ...ldField,
@@ -38,56 +38,71 @@ frappe.ui.form.on('Design', {
                 || ldField.default || ldField.min || ""
         }));
     },
+
+    // Gets the saved value of a specific attribute from the child table
     fnMappingSavedValues(frm, fieldname) {
-        const laValues = (frm.doc.design_attributes || []).find(
+        const ldValues = (frm.doc.design_attributes || []).find(
             ldValue => ldValue.attribute.toLowerCase() === fieldname.toLowerCase()
         );
-        return laValues ? laValues.attribute_value : null;
+        return ldValues ? ldValues.attribute_value : null;
     },
+    
+    // Defines the HTML structure for dynamic field rendering
     fnGenerateTemplateHtml() {
         return `
-            <div class="design-grid-wrapper" style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 10px; column-gap: 24px;"> 
-                {% for (var i = 0; i < LaFields.length; i++) {
-                    var field = LaFields[i];
+            <style id="custom-slider-style">
+                .range-field {
+                    -webkit-appearance: none; width: 100%; height: 5px;
+                    background: #ccc; border-radius: 4px; outline: none; margin-top: 6px;
+                }
+                .range-field::-webkit-slider-thumb,
+                .range-field::-moz-range-thumb {
+                    border-radius: 8%; background: black; cursor: pointer;
+                }
+            </style>
+
+            <div class="design-grid-wrapper" style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 10px; column-gap: 30px;"> 
+                {% for (var laIndex = 0; laIndex < LaFields.length; laIndex++) {
+                    var ldField = LaFields[laIndex];
                 %}
                 <div class="form-column">
-                    <div class="frappe-control input-max-width" data-fieldname="{%= field.fieldname %}">
+                    <div class="frappe-control input-max-width" data-fieldname="{%= ldField.fieldname %}">
                         <div class="form-group" style="margin-bottom: 6px;">
-                            <label class="control-label" style="margin-bottom: 4px;">{%= field.label %}</label>
+                            <label class="control-label" style="margin-bottom: 4px;">{%= ldField.label %}</label>
                             <div class="control-input flex flex-column" style="gap:6px;">
-                                {% if (field.numeric_values) { %}
+                                {% if (ldField.numeric_values) { %}
                                     <input type="range"
                                         class="range-field"
-                                        data-fieldname="{%= field.fieldname %}"
-                                        min="{%= field.min %}"
-                                        max="{%= field.max %}"
-                                        step="{%= field.step %}"
-                                        value="{%= field.lSavedValue %}" 
+                                        data-fieldname="{%= ldField.fieldname %}"
+                                        min="{%= ldField.min %}"
+                                        max="{%= ldField.max %}"
+                                        step="{%= ldField.step %}"
+                                        value="{%= ldField.lSavedValue %}" 
                                         style="width: 100%; accent-color: black;" />
 
                                     <input type="number"
                                         class="number-field input-with-feedback form-control ellipsis"
-                                        data-fieldname="{%= field.fieldname %}"
-                                        min="{%= field.min %}"
-                                        max="{%= field.max %}"
-                                        step="{%= field.step %}"
-                                        value="{%= field.lSavedValue %}"
+                                        data-fieldname="{%= ldField.fieldname %}"
+                                        min="{%= ldField.min %}"
+                                        max="{%= ldField.max %}"
+                                        step="{%= ldField.step %}"
+                                        value="{%= ldField.lSavedValue %}"
                                         style="background-color: #F3F3F3; border-radius: 6px; padding: 6px;" />
 
-                                    <small id="error_{%= field.fieldname %}" class="text-danger" style="margin-top: -4px;"></small>
+                                    <small id="error_{%= ldField.fieldname %}" class="text-danger" style="margin-top: -4px;"></small>
                                     <small class="text-muted">
-                                        From Range: {%= field.min %}, To Range: {%= field.max %}, Increment: {%= field.step %}
+                                        From Range: {%= ldField.min %}, To Range: {%= ldField.max %}, Increment: {%= ldField.step %}
                                     </small>
                                 {% } else { %}
                                     <select class="select-field form-control input-with-feedback ellipsis"
-                                            data-fieldname="{%= field.fieldname %}"
+                                            data-fieldname="{%= ldField.fieldname %}"
                                             style="background-color: #F3F3F3; border-radius: 6px; padding: padding: revert-layer;">
-                                        <option disabled {%= !field.lSavedValue ? 'selected' : '' %}>Select {%= field.label %}</option>
-                                        {% for (var j = 0; j < field.options.length; j++) {
-                                            var opt = field.options[j];
+                                        <option disabled {%= !ldField.lSavedValue ? 'selected' : '' %}>Select {%= ldField.label %}</option>
+                                        {% for (var laOption = 0; laOption < ldField.options.length; laOption++) {
+                                            var ldOpt = ldField.options[laOption];
                                         %}
-                                            <option value="{%= opt.value %}" {%= opt.value == field.lSavedValue ? 'selected' : '' %}>
-                                                {%= opt.label %}
+                                            <option value="{%= ldOpt.value %}" {%= ldOpt.value == ldField.lSavedValue ? 'selected' : '' %}>
+                                                {%= ldOpt.label %}
                                             </option>
                                         {% } %}
                                     </select>
@@ -101,52 +116,31 @@ frappe.ui.form.on('Design', {
         `;
     },
 
+
     fnRenderDynamicSlider(frm, iRenderHtml, iaFields) {
-        console.log("");
         frm.set_df_property("dynamic_fields", "options", iRenderHtml);
-           if (!document.getElementById("custom-slider-style")) {
-            const css = `
-                .range-field {
-                    -webkit-appearance: none;
-                    width: 100%;
-                    height: 5px;
-                    background: #ccc;
-                    border-radius: 4px;
-                    outline: none;
-                    margin-top: 6px;
-                }
-                .range-field::-webkit-slider-thumb,
-                .range-field::-moz-range-thumb {
-                    border-radius: 8%;
-                    background: black;
-                    cursor: pointer;
-                }
-            `;
-            $("<style id='custom-slider-style'>").text(css).appendTo("head");
-        }
         frappe.after_ajax(() => {
-            const $wrapper = $(frm.fields_dict["dynamic_fields"].$wrapper.get(0));
+            const $elDynamicWrapper = $(frm.fields_dict["dynamic_fields"].$wrapper.get(0));
 
-            iaFields.forEach(field => {
-                const { fieldname, numeric_values } = field;
-
+            iaFields.forEach(ldField => {
+                const { fieldname, numeric_values } = ldField;
                 if (numeric_values) {
-                    const $range = $wrapper.find(`.range-field[data-fieldname="${fieldname}"]`);
-                    const $number = $wrapper.find(`.number-field[data-fieldname="${fieldname}"]`);
-                    const $error = $number.next('.text-danger');
+                    const $elRange = $elDynamicWrapper.find(`.range-field[data-fieldname="${fieldname}"]`);
+                    const $elNumber = $elDynamicWrapper.find(`.number-field[data-fieldname="${fieldname}"]`);
+                    const $elError = $elNumber.next('.text-danger');
 
-                    const syncFields = ($src, $target) => {
-                        const value = parseFloat($src.val());
-                        const isValid = frm.events.fnValidateAndUpdateNumericInput(
-                            frm, fieldname, value, $src.get(0), $target.get(0), $error.get(0)
+                    const fnsyncFields = ($elSrc, $elTarget) => {
+                        const lValue = parseFloat($elSrc.val());
+                        const lIsValid = frm.events.fnValidateAndUpdateNumericInput(
+                            frm, fieldname, lValue, $elSrc.get(0), $elTarget.get(0), $elError.get(0)
                         );
-                        if (isValid) $target.val(value);
+                        if (lIsValid) $elTarget.val(lValue);
                     };
 
-                    $number.on("input", () => syncFields($number, $range));
-                    $range.on("input", () => syncFields($range, $number));
+                    $elNumber.on("input", () => fnsyncFields($elNumber, $elRange));
+                    $elRange.on("input", () => fnsyncFields($elRange, $elNumber));
                 } else {
-                    $wrapper.find(`.select-field[data-fieldname="${fieldname}"]`)
+                    $elDynamicWrapper.find(`.select-field[data-fieldname="${fieldname}"]`)
                         .on("change", function () {
                             frm.events.fnUpdateDesignAttribute(frm, fieldname, this.value);
                         });
@@ -154,42 +148,46 @@ frappe.ui.form.on('Design', {
             });
         });
     },
-    fnUpdateDesignAttribute(frm, fieldname, value) {
+
+     // Updates the design_attributes child table with the selected value
+    fnUpdateDesignAttribute(frm, iFieldname, iValue) {
         let laRows = (frm.doc.design_attributes || []).find(
-            ldRow => ldRow.attribute.toLowerCase() === fieldname.toLowerCase()
+            ldRow => ldRow.attribute.toLowerCase() === iFieldname.toLowerCase()
         );
         if (!laRows) {
-            laRows = frm.add_child('design_attributes', { attribute: fieldname });
+            laRows = frm.add_child('design_attributes', { attribute: iFieldname });
         }
-        frappe.model.set_value(laRows.doctype, laRows.name, 'attribute_value', value);
+        frappe.model.set_value(laRows.doctype, laRows.name, 'attribute_value', iValue);
     },
-    fnValidateAndUpdateNumericInput(frm, fieldname, value, sourceElement, targetElement, errorEl) {
-        const min = parseFloat(sourceElement.getAttribute("min"));
-        const max = parseFloat(sourceElement.getAttribute("max"));
-        const step = parseFloat(sourceElement.getAttribute("step"));
-        let errorMessage = "";
 
-        if (isNaN(value)) {
-            errorMessage = __("Value is required");
-        } else if (value < min || value > max) {
-            errorMessage = __(`Value must be between ${min} and ${max}`);
-        } else if (Math.abs(((value - min) / step) % 1) > 1e-6) {
-            errorMessage = __(`Value should increment by ${step}`);
+    // Validates the numeric input value and shows an error message if it is out of range
+    // Removes the field from the error list if the value is valid
+    fnValidateAndUpdateNumericInput(frm, iFieldname, iValue, elSourceElement, elTargetElement, elErrorElement) {
+        const lMin = parseFloat(elSourceElement.getAttribute("min"));
+        const lMax = parseFloat(elSourceElement.getAttribute("max"));
+        const lStep = parseFloat(elSourceElement.getAttribute("step"));
+        let lErrorMessage = "";
+
+        if (isNaN(iValue)) {
+            lErrorMessage = __("Value is required");
+        } else if (iValue < lMin || iValue > lMax) {
+            lErrorMessage = __(`Value must be between ${lMin} and ${lMax}`);
+        } else if (Math.abs(((iValue - lMin) / lStep) % 1) > 1e-6) {
+            lErrorMessage = __(`Value should increment by ${lStep}`);
         }
 
-        if (errorMessage) {
-            errorEl.innerText = errorMessage;
-            InvalidValue.add(fieldname);
+        if (lErrorMessage) {
+            elErrorElement.innerText = lErrorMessage;
+            lInvalidValue.add(iFieldname);
             return false;
         }
 
-        errorEl.innerText = "";
-        targetElement.value = value;
-        InvalidValue.delete(fieldname);
-        frm.events.fnUpdateDesignAttribute(frm, fieldname, value);
+        elErrorElement.innerText = "";
+        elTargetElement.value = iValue;
+        lInvalidValue.delete(iFieldname);
+        frm.events.fnUpdateDesignAttribute(frm, iFieldname, iValue);
         return true;
     }
-
 });
 
 
