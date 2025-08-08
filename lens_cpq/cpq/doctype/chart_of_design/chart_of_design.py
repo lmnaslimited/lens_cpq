8a4c39a4239d48a825c4ffb8d343a9450fc2c2d3
Krishna <krishnagovarthini-lmnas>
2025-08-08 09:51:36 +0000

    fix:doctype view and server script

--- lens_cpq/cpq/doctype/chart_of_design/chart_of_design.py
+++ lens_cpq/cpq/doctype/chart_of_design/chart_of_design.py
@@ -19,26 +19,21 @@ def fn_generate_chart_of_design_name(i_parent_name, i_attribute):
 
     # Check if parent exists as a Chart of Design node
     if l_cod_exists:
-        # Fetch the Chart of Design parent document
-        ld_cod_doc = frappe.get_doc("Chart of Design", i_parent_name)
-
-        # Get abbreviation from the parent Chart of Design document
-        l_abbr = ld_cod_doc.root_abbr
-
-        # Get root node reference from the parent Chart of Design document
-        l_root_node = ld_cod_doc.root_node 
+        [l_abbr, l_root_node] = frappe.db.get_value(
+            "Chart of Design", 
+            {"name": i_parent_name},
+            ["root_abbr", "root_node"]
+        )
 
     # Check if parent exists as a Plant Floor node
     elif l_plant_floor_exists:
-        # Fetch the Plant Floor parent document
-        ld_plant_floor_doc = frappe.get_doc("Plant Floor", i_parent_name)
-
-        # Get custom abbreviation from the Plant Floor document
-        l_abbr = ld_plant_floor_doc.custom_abbreviation
-
-        # Since parent is Plant Floor, root node is the parent itself
+        [l_abbr] = frappe.db.get_value(
+            "Plant Floor",
+            {"name": i_parent_name},
+            ["custom_abbreviation"]
+        )
         l_root_node = i_parent_name
-
+ 
     else:
         # Throw error if parent not found in either doctype
         frappe.throw(_("Parent node not found in Chart Of Design or Plant Floor"))
