10a56d9f02a7827ff5da17c9c955bc778e8014ea
Krishna <krishnagovarthini-lmnas>
2025-11-25 09:32:54 +0000

    Error Validation

--- lens_cpq/cpq/doctype/design/design.py
+++ lens_cpq/cpq/doctype/design/design.py
@@ -3,21 +3,32 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.exceptions import DuplicateEntryError
 
 class Design(Document):
-
+	
 	"""
 		Method runs during creation and when `is_template` is enabled,
 		it replaces the default system-generated name with `template_name`.
 
-        Raises: frappe.ValidationError: If a Design record already exists with the 
-        same Template Name.
-    """
+		Raises:
+			frappe.ValidationError: If a Design record already exists with the
+			same Template Name.
+	"""
 	def autoname(self):
 		if self.is_template and self.template_name:
-			# Check for duplicate design document name
-			if frappe.db.exists("Design", self.template_name):
-				frappe.throw(f"Design Template '{self.template_name}' already exists.")
+
+			try:
+				# Check for duplicate based on docname
+				if frappe.db.exists("Design", self.template_name):
+					frappe.throw(
+						f"Design Template '{self.template_name}' already exists.",
+						DuplicateEntryError
+					)
+
+			except DuplicateEntryError:
+				frappe.msgprint("A duplicate entry was found. Skipping.")
+				return
 
 			# Set the document name
 			self.name = self.template_name
